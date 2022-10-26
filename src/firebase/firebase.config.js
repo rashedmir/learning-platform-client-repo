034da0a2d77bad70af609daacaf1b23a1687e26e
rashedmir <rashedmir89@gmail.com>
2022-10-26 10:41:55 +0600

    firebase.config.js added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCp_Tx17tyDCbRhFk0Q3UKTyvuUS0iIG1E",
+    authDomain: "eduonline-b8c80.firebaseapp.com",
+    projectId: "eduonline-b8c80",
+    storageBucket: "eduonline-b8c80.appspot.com",
+    messagingSenderId: "248884242161",
+    appId: "1:248884242161:web:1c0cfb3a2d145c11e2ab50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
